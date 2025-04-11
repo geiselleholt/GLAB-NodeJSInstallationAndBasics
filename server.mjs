d959047482309899f9a9d66501b062452a867256
Geiselle Holt <geiselleholt@hotmail.com>
2025-04-11 13:59:54 -0600

    changed switched staement to change colors on each page

--- server.mjs
+++ server.mjs
@@ -11,27 +11,29 @@ const port = 3000;
 // });
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    // res.write('<h1 style="color: red">Hello World!</h1>');
-    // res.write('<p>I wonder what else we can send...</p>');
-    switch (req.url) { //Observe the url AFTER the main routing (http://localhost:3000)
-        case '/':
-            res.write('<h1 style="color: red">Hello Home Page!</h1>');
-          break;
-        case '/animal':
-          res.write('I have the best Cat(s)');
-          break;
-        case '/test':
-          res.write('This is the test route');
-          break;
-        default:
-          res.statusCode = 404;
-          res.write('404 Page Not Found');
-          break;
-      }
-    res.end();
-  });
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  // res.write('<h1 style="color: red">Hello World!</h1>');
+  // res.write('<p>I wonder what else we can send...</p>');
+  switch (
+    req.url //Observe the url AFTER the main routing (http://localhost:3000)
+  ) {
+    case "/":
+      res.write('<h1 style="color: red">Hello Home Page!</h1>');
+      break;
+    case "/pageTwo":
+      res.write(`<h1 style="color: green">Hello Page 2</h1>`);
+      break;
+    case "/pageThree":
+      res.write(`<h1 style="color: blue">Hello Page 3</h1>`);
+      break;
+    default:
+      res.statusCode = 404;
+      res.write("404 Page Not Found");
+      break;
+  }
+  res.end();
+});
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
