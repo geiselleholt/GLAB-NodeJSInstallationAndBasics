6aa96422dfca40bf5094932647dc770f1e195a1b
Geiselle Holt <geiselleholt@hotmail.com>
2025-04-11 13:52:22 -0600

    Part 4 and 5

---
+++ server.mjs
@@ -0,0 +1,38 @@
+import http from "http";
+// const http = require('http');
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   res.end("Hello World!\n");
+// });
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    // res.write('<h1 style="color: red">Hello World!</h1>');
+    // res.write('<p>I wonder what else we can send...</p>');
+    switch (req.url) { //Observe the url AFTER the main routing (http://localhost:3000)
+        case '/':
+            res.write('<h1 style="color: red">Hello Home Page!</h1>');
+          break;
+        case '/animal':
+          res.write('I have the best Cat(s)');
+          break;
+        case '/test':
+          res.write('This is the test route');
+          break;
+        default:
+          res.statusCode = 404;
+          res.write('404 Page Not Found');
+          break;
+      }
+    res.end();
+  });
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
